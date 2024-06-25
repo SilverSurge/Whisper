672dbab441f6b923e40081bb935c63791c66add1
SilverSurge <deepkumarpatel471@gmail.com>
2024-06-25 20:26:40 +0530

    completed

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,64 +1,68 @@
 import { Conversation } from "../models/conversationModel.js";
 import { Message } from "../models/messageModel.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 // logic for sending message
 export const sendMessage = async (req, res) => {
-    try {
+  try {
+    // get the message details
+    const senderId = req.id; // from authentication middleware
+    const receiverId = req.params.id;
+    const { message } = req.body;
 
-        // get the message details
-        const senderId = req.id;            // from authentication middleware
-        const receiverId = req.params.id;
-        const { message } = req.body;
+    // retrieve the conversation
+    let gotConversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
 
-        
-        // retrieve the conversation
-        let gotConversation = await Conversation.findOne({
-            participants: { $all: [senderId, receiverId] },
-        });
-
-
-        // if no conversation prior, then create a new conversation
-        if (!gotConversation) {
-            gotConversation = await Conversation.create({
-                participants: [senderId, receiverId]
-            })
-        }
-
-        // create a new message and append the id to the message list
-        const newMessage = await Message.create({
-            senderId,
-            receiverId,
-            message
-        });
-        if (newMessage) {
-            gotConversation.messages.push(newMessage._id);
-        };
-        await gotConversation.save();
-
-        return res.status(201).json({
-            message: "message sent successfully"
-        })
-
-        // SOCKET IO [PENDING]
-    } catch (error) {
-        console.log(error);
+    // if no conversation prior, then create a new conversation
+    if (!gotConversation) {
+      gotConversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
     }
-}
+
+    // create a new message and append the id to the message list
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      message,
+    });
+    if (newMessage) {
+      gotConversation.messages.push(newMessage._id);
+    }
+    // await gotConversation.save();
+    // await newMessage.save();
+    await Promise.all([gotConversation.save(), newMessage.save()]);
+
+    // SOCKET IO [PENDING]
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+        io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+    return res.status(201).json({
+      message: "message sent successfully",
+      newMessage,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getMessage = async (req, res) => {
-    try {
+  try {
+    // get conversation details
+    const receiverId = req.params.id;
+    const senderId = req.id;
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    }).populate("messages");
+    // messages is initially a list of ids
+    // the populate method, fetch the complete messages from the ids
 
-        // get conversation details
-        const receiverId = req.params.id;
-        const senderId = req.id;
-        const conversation = await Conversation.findOne({
-            participants: { $all: [senderId, receiverId] }
-        }).populate("messages");
-        // messages is initially a list of ids
-        // the populate method, fetch the complete messages from the ids
-
-        return res.status(200).json(conversation?.messages);
-    } catch (error) {
-        console.log(error);
-    }
-}
+    return res.status(200).json(conversation?.messages);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-const app = express();
+import { app,server } from "./socket/socket.js";
+// const app = express();
 
 const PORT = process.env.PORT || 5000;
 // middleware
@@ -26,7 +27,8 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/message", messageRoute)
 // http://localhost:8080/api/v1/user/register
 
-app.listen(PORT, () => {
+// app.listen -> server.listen
+server.listen(PORT, () => {
     connectDB();
     console.log(`server listening on port ${PORT}`);
 })
